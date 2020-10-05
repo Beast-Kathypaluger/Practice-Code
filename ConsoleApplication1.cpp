69bd7330e56f2b9b4fb1551f327d8c03421bc813
Beast-Kathypaluger <alima1220@outlook.com>
2020-10-05 15:35:36 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#define MAX 100
+int main(void)
+{
+	int count = MAX + 1;
+	while (--count > 0)
+	{
+		printf("\a%d bottles of spring water on the wall,"
+			"%d bottles of spring water!\n", count, count);
+		printf("Take one down and pass it around,\n");
+		printf("%d bottles of spring water!\n\n", count - 1);
+	}
+	getchar();
+	return 0;
+}
